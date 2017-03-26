36d312f14238feb5f5e8ca8956779ff7ab0a427d
Vijay Koju <vijaykoju@gmail.com>
2017-03-26 02:16:22 +0000

    new test

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+import datetime
+import quandl
+from bokeh.charts import Scatter, output_file
 
 app = Flask(__name__)
 
@@ -8,6 +11,26 @@ def main():
 
 @app.route('/index')
 def index():
+  today = datetime.date.today()
+  last_month = today.month - 1
+
+  if last_month < 10:
+    sd = '0'+str(last_month)
+    ed = '0'+str(today.month)
+  else:
+    sd = str(last_month)
+    ed = str(today.month)
+
+  start_date = str(today.year)+'-'+sd+'-'+'01'
+  end_date = str(today.year)+'-'+ed+'-'+'01'
+
+  quandl.ApiConfig.api_key = 'dvzXzx3iSej1nbvhG_HU'
+
+  ticker = 'AAPL'
+  data = quandl.get_table('WIKI/PRICES', paginate=True, ticker=[ticker], date={'gte': start_date, 'lt': end_date}, qopts={'columns':['ticker', 'date', 'close']})
+
+  p = Scatter(data, x="date", y="close", title="Closing stock", xlabel="Previous month date", ylabel="Closing stock")
+  output_file("templates/Scatter_charts_closing_stock.html")
   return render_template('Scatter_charts_closing_stock.html')
 
 if __name__ == '__main__':
